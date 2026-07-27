b6b4e65b8a84c9ffc4de0b44c6ce0b5c32977ed1
Edna Katherine <penarodriguezednakaterine@gmail.com>
2026-07-26 21:04:22 -0500

    Eliminar campo foto del modelo y actualizar migraciones ademas de contactos

--- configuracion_mentalidad/settings.py
+++ configuracion_mentalidad/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-y2kn@ju54k_)f@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.onrender.com').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] + os.environ.get('ALLOWED_HOSTS', '.onrender.com').split(',')
+
 
 
 # Application definition
